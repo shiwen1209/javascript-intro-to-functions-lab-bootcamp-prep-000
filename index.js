2604d837e51625dd7e2837b11c804d286eb5eddb
Wendy She <whshiwen@gmail.com>
2018-05-15 18:29:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log(`${string.toUpperCase()}`)}
 function logWhisper(string) {console.log(`${string.toLowerCase()}`)}
 function sayHiToGrandma(string){
-if(string="I love you, Grandma."){
-return("I love you, too.")}}
+if(string==="I love you, Grandma."){
+console.log("I love you, too.");
+}else if(string===string.toUpperCase()){
+console.log("YES INDEED!");
+}else{console.log("I can't hear you!");
+}}
