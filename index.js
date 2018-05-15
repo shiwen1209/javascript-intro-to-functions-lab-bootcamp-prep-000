201b12ab7621bf32bfe1dc098ef897394babab99
Wendy She <whshiwen@gmail.com>
2018-05-15 15:34:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ function logWhisper(string) {console.log(`${string.toLowerCase()}`)}
 function sayHiToGrandma(string){
 if(string="I love you, Grandma."){
 return("I love you, too.")}}
+function sayHiToGrandma(string) {
+if(string=string.toUpperCase()){console.log(`YES INDEED!`)}}
